aa7d39a292e67e30403d494f05dcd923064591d3
Walden <walden980302@outlook.com>
2023-03-26 13:17:15 -0400

    Update tracker.py format

--- tracker.py
+++ tracker.py
@@ -32,18 +32,13 @@ Note the actual implementation of the ORM is hidden and so it
 could be replaced with PostgreSQL or Pandas or straight python lists
 
 '''
-
-from transaction import Transaction
 import sys
 import datetime
 import os
+from transaction import Transaction
 
 
 # here are some helper functions ...
-
-  # todo show categories
-    # todo add category
-# todo modify category
 
 def print_usage():
     ''' print an explanation of how to use this command '''
@@ -58,7 +53,7 @@ def print_usage():
             [8] summarize_transactions by category
             [9] print_this_menu
             '''
-        )
+          )
 
 # def print_transactions(transactions):
 #     ''' print the transactions items '''
@@ -83,65 +78,70 @@ def print_transactions(transactions):
         print('no transactions to print')
         return
     print('\n')
-    if transactions[0] != '[1] quit':   # if called method is print_this_menu(), ignore table header
-        print("%-10s %-10s %-20s %-20s %-30s"%('item', 'amount', 'category', 'date', 'description'))
+    # if called method is print_this_menu(), ignore table header
+    if transactions[0] != '[1] quit':
+        print("%-10s %-10s %-20s %-20s %-30s" %
+              ('item', 'amount', 'category', 'date', 'description'))
     print('-'*75)
     for item in transactions:
         if isinstance(item, str):
             print(item)
         else:
-            values = tuple(item.values()) #(item, amount, category, date, description)
-            print("%-10s %-10s %-20s %-20s %-30s"%values)
+            # (item, amount, category, date, description)
+            values = tuple(item.values())
+            print("%-10s %-10s %-20s %-20s %-30s" % values)
+
 
 def process_args(arglist):
     ''' examine args and make appropriate calls to Transaction'''
     # transaction = Transaction('transaction')
     transaction = Transaction(os.getenv('HOME')+'/transaction.db')
-    if arglist==[]:
+    if arglist == []:
         print_usage()
-    elif arglist[0]=="show_transactions":
+    elif arglist[0] == "show_transactions":
         print_transactions(transaction.show_transactions())
-    elif arglist[0]=="summarize_transactions_by_date":
+    elif arglist[0] == "summarize_transactions_by_date":
         print_transactions(transaction.summarize_transactions_by_date())
-    elif arglist[0]=="summarize_transactions_by_month":
+    elif arglist[0] == "summarize_transactions_by_month":
         print_transactions(transaction.summarize_transactions_by_month())
-    elif arglist[0]=="summarize_transactions_by_year":
+    elif arglist[0] == "summarize_transactions_by_year":
         print_transactions(transaction.summarize_transactions_by_year())
-    elif arglist[0]=="summarize_transactions_by_category":
+    elif arglist[0] == "summarize_transactions_by_category":
         print_transactions(transaction.summarize_transactions_by_category())
-    elif arglist[0]=='add_transaction':
-        if len(arglist)!=5:
+    elif arglist[0] == 'add_transaction':
+        if len(arglist) != 5:
             print('Invalid input for add_transaction')
             print_usage()
         else:
-            transaction.add_transaction(arglist[1], arglist[2], arglist[3],  ' '.join(arglist[4:]))
-    elif arglist[0]=='print_this_menu':
+            transaction.add_transaction(
+                arglist[1], arglist[2], arglist[3],  ' '.join(arglist[4:]))
+    elif arglist[0] == 'print_this_menu':
         print_transactions(transaction.print_this_menu())
-    elif arglist[0]=='delete_transaction':
-        if len(arglist)!= 2:
+    elif arglist[0] == 'delete_transaction':
+        if len(arglist) != 2:
             print_usage()
         else:
-            transaction.delete(arglist[1]) # delete by id
-    elif arglist[0]=='quit':
+            transaction.delete_transaction(arglist[1])  # delete by id
+    elif arglist[0] == 'quit':
         return
     else:
-        print(arglist,"is not implemented")
+        print(arglist, "is not implemented")
         print_usage()
 
 
 def toplevel():
     ''' read the command args and process them'''
-    if len(sys.argv)==1:
-        # they didn't pass any arguments, 
+    if len(sys.argv) == 1:
+        # they didn't pass any arguments,
         # so prompt for them in a loop
         print_usage()
         args = []
-        while args!=['']:
+        while args != ['']:
             args = input("command> ").split(' ')
-            if args[0]=='add':
+            if args[0] == 'add':
                 # join everyting after the name as a string
                 # args = ['add',args[1]," ".join(args[2:])]
-                args = ['add',args[1],args[2],args[3], ' '.join(args[3:])]
+                args = ['add', args[1], args[2], args[3], ' '.join(args[3:])]
             process_args(args)
             print('-'*75+'\n'*3)
     else:
@@ -150,5 +150,5 @@ def toplevel():
         process_args(args)
         print('-'*75+'\n'*3)
 
-toplevel()
 
+toplevel()
